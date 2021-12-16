e72ead728003a8eea5b31051dd48348d460342df
Vignesh <vigneshsrinivasan18@gmail.com>
2021-12-17 03:10:53 +0530

    Added 6th TC

---
+++ src/test/java/tests/TC06.java
@@ -0,0 +1,22 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+import pages.HomePage;
+
+public final class TC06 extends BaseTest {
+
+    @Test()
+    public void clearItemsInCart() {
+        HomePage homePage = new HomePage();
+        CartPage cartPage = homePage.clickAddToCart("1")
+                .waitForCartPageToLoad();
+
+        int cartTotalPriceText = cartPage.getCartTotalPriceText();
+        int bagQuantityCountText = cartPage.getBagQuantityCountText();
+
+        Assert.assertEquals(cartTotalPriceText,0,"Mismatch with the price");
+        Assert.assertEquals(bagQuantityCountText,0,"Mismatch with the quantity");
+    }
+}
